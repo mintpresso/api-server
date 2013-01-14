608f4f22ac4d25e4ff1588d1461c14b132469ede
Jinhyuk Lee <eces@mstock.org>
2013-01-14 17:56:02 +0900

    Set nice default error page

---
+++ app/Global.scala
@@ -0,0 +1,38 @@
+import play.api._
+import play.api.mvc._
+import play.api.mvc.Results._
+import play.api.libs.concurrent._
+import play.api.Play.current
+
+import anorm._
+
+object Global extends GlobalSettings {
+
+  override def onStart(app: Application) {
+    // Logger.info("Application has started")
+  }  
+  
+  override def onStop(app: Application) {
+    // Logger.info("Application shutdown...")
+  }
+
+  override def onError(request: RequestHeader, ex: Throwable) = {
+    InternalServerError(
+      views.html.error(500, request)
+    )
+  }
+
+  override def onHandlerNotFound(request: RequestHeader): Result = {
+    NotFound(
+      views.html.error(404, request)
+    )
+  }
+
+  override def onBadRequest(request: RequestHeader, error: String) = {
+    Logger.info("BadRequest: " + error)
+    BadRequest(
+    	views.html.error(400, request)
+    )
+  }
+    
+}
