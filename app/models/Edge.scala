ca75bc137ef680d99086d4c5faba0b40a97feec9
Jinhyuk Lee <eces@mstock.org>
2013-01-21 22:26:38 +0900

    Add support for optional verb type

--- app/models/Edge.scala
+++ app/models/Edge.scala
@@ -34,60 +34,123 @@ object Edge {
 
   def find(verb: Option[String], args: (String, Long)*): List[Edge] = {
     DB.withConnection { implicit conn =>
-      args.length match {
-        case 1 =>
-          SQL(
-            """
-            select * from edges where %1$s = {f1} 
-            """.format(args(0)._1)
-          ).on(
-            'f1 -> args(0)._2
-          ).as(parser *)
-        case 2 =>
-          SQL(
-            """
-            select * from edges where %1$s = {f1} and %2$s = {f2} 
-            """.format(args(0)._1, args(1)._1)
-          ).on(
-            'f1 -> args(0)._2,
-            'f2 -> args(1)._2
-          ).as(parser *)
-        case 3 =>
-          SQL(
-            """
-            select * from edges where %1$s = {f1} and %2$s = {f2} and %3$s = {f3} 
-            """.format(args(0)._1, args(1)._1, args(2)._1)
-          ).on(
-            'f1 -> args(0)._2,
-            'f2 -> args(1)._2,
-            'f3 -> args(2)._2
-          ).as(parser *)
-        case 4 =>
-          SQL(
-            """
-            select * from edges where %1$s = {f1} and %2$s = {f2} and %3$s = {f3} and %4$s = {f4} 
-            """.format(args(0)._1, args(1)._1, args(2)._1, args(3)._1)
-          ).on(
-            'f1 -> args(0)._2,
-            'f2 -> args(1)._2,
-            'f3 -> args(2)._2,
-            'f4 -> args(3)._2
-          ).as(parser *)
-        case 5 =>
-          SQL(
-            """
-            select * from edges where %1$s = {f1} and %2$s = {f2} and %3$s = {f3} and %4$s = {f4} and %5$s = {f5}
-            """.format(args(0)._1, args(1)._1, args(2)._1, args(3)._1, args(4)._1)
-          ).on(
-            'f1 -> args(0)._2,
-            'f2 -> args(1)._2,
-            'f3 -> args(2)._2,
-            'f4 -> args(3)._2,
-            'f5 -> args(4)._2
-          ).as(parser *)
-        case _ =>
-          Logger.info("Edge.find: requested with exceed 5 parameters. ")
-          List()
+      verb map { v =>
+        args.length match {
+          case 1 =>
+            SQL(
+              """
+              select * from edges where %1$s = {f1} and v = {v} 
+              """.format(args(0)._1)
+            ).on(
+              'v -> v,
+              'f1 -> args(0)._2
+            ).as(parser *)
+          case 2 =>
+            SQL(
+              """
+              select * from edges where %1$s = {f1} and %2$s = {f2} and v = {v}
+              """.format(args(0)._1, args(1)._1)
+            ).on(
+              'v -> v,
+              'f1 -> args(0)._2,
+              'f2 -> args(1)._2
+            ).as(parser *)
+          case 3 =>
+            SQL(
+              """
+              select * from edges where %1$s = {f1} and %2$s = {f2} and %3$s = {f3} and v = {v}
+              """.format(args(0)._1, args(1)._1, args(2)._1)
+            ).on(
+              'v -> v,
+              'f1 -> args(0)._2,
+              'f2 -> args(1)._2,
+              'f3 -> args(2)._2
+            ).as(parser *)
+          case 4 =>
+            SQL(
+              """
+              select * from edges where %1$s = {f1} and %2$s = {f2} and %3$s = {f3} and %4$s = {f4} and v = {v}
+              """.format(args(0)._1, args(1)._1, args(2)._1, args(3)._1)
+            ).on(
+              'v -> v,
+              'f1 -> args(0)._2,
+              'f2 -> args(1)._2,
+              'f3 -> args(2)._2,
+              'f4 -> args(3)._2
+            ).as(parser *)
+          case 5 =>
+            SQL(
+              """
+              select * from edges where %1$s = {f1} and %2$s = {f2} and %3$s = {f3} and %4$s = {f4} and %5$s = {f5} and v = {v}
+              """.format(args(0)._1, args(1)._1, args(2)._1, args(3)._1, args(4)._1)
+            ).on(
+              'v -> v,
+              'f1 -> args(0)._2,
+              'f2 -> args(1)._2,
+              'f3 -> args(2)._2,
+              'f4 -> args(3)._2,
+              'f5 -> args(4)._2
+            ).as(parser *)
+          case _ =>
+            Logger.info("Edge.find: requested with exceed 5 parameters. ")
+            List()
+        }
+      } getOrElse {
+        args.length match {
+          case 1 =>
+            SQL(
+              """
+              select * from edges where %1$s = {f1} 
+              """.format(args(0)._1)
+            ).on(
+              'f1 -> args(0)._2
+            ).as(parser *)
+          case 2 =>
+            SQL(
+              """
+              select * from edges where %1$s = {f1} and %2$s = {f2} 
+              """.format(args(0)._1, args(1)._1)
+            ).on(
+              'f1 -> args(0)._2,
+              'f2 -> args(1)._2
+            ).as(parser *)
+          case 3 =>
+            SQL(
+              """
+              select * from edges where %1$s = {f1} and %2$s = {f2} and %3$s = {f3} 
+              """.format(args(0)._1, args(1)._1, args(2)._1)
+            ).on(
+              'f1 -> args(0)._2,
+              'f2 -> args(1)._2,
+              'f3 -> args(2)._2
+            ).as(parser *)
+          case 4 =>
+            SQL(
+              """
+              select * from edges where %1$s = {f1} and %2$s = {f2} and %3$s = {f3} and %4$s = {f4} 
+              """.format(args(0)._1, args(1)._1, args(2)._1, args(3)._1)
+            ).on(
+              'f1 -> args(0)._2,
+              'f2 -> args(1)._2,
+              'f3 -> args(2)._2,
+              'f4 -> args(3)._2
+            ).as(parser *)
+          case 5 =>
+            SQL(
+              """
+              select * from edges where %1$s = {f1} and %2$s = {f2} and %3$s = {f3} and %4$s = {f4} and %5$s = {f5}
+              """.format(args(0)._1, args(1)._1, args(2)._1, args(3)._1, args(4)._1)
+            ).on(
+              'f1 -> args(0)._2,
+              'f2 -> args(1)._2,
+              'f3 -> args(2)._2,
+              'f4 -> args(3)._2,
+              'f5 -> args(4)._2
+            ).as(parser *)
+          case _ =>
+            Logger.info("Edge.find: requested with exceed 5 parameters. ")
+            List()
+        }
       }
     }
   }
