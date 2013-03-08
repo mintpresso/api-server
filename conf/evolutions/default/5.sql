959f8f9ae5cce02670cb1d51d39f194e859561c1
Kang HyoJun <admire9@gmail.com>
2013-03-08 21:02:17 +0900

    Modify point_types to pointType

--- conf/evolutions/default/5.sql
+++ conf/evolutions/default/5.sql
@@ -1,10 +1,10 @@
 # --- !Ups
 
-CREATE TABLE point_types (
+CREATE TABLE pointTypes (
   id bigint(20) NOT NULL PRIMARY KEY AUTO_INCREMENT,
   name varchar(255) NOT NULL
 );
 
 # --- !Downs
 
-DROP TABLE point_types;
+DROP TABLE pointTypes;
