007fba5b88910e77a669419d06590c232013cd85
Jinhyuk Lee <eces@mstock.org>
2013-04-03 02:00:45 +0900

    Modify token process to use graph

--- app/controllers/Secured.scala
+++ app/controllers/Secured.scala
@@ -6,17 +6,19 @@ import play.api.data.Forms._
 import play.api.data._
 import play.api.Play.current
 import play.api.libs._
-import models.Account
 
+import play.api.libs.concurrent._
+import java.util.concurrent._
+import play.api.libs.json._
+
+import com.mintpresso._
+import play.api.libs.concurrent.Execution.Implicits._
 /** Uncomment the following lines as needed **/
 /**
 import play.api.libs.iteratee._
-import play.api.libs.concurrent._
-import java.util.concurrent._
 import scala.concurrent.stm._
 import akka.util.duration._
 import play.api.cache._
-import play.api.libs.json._
 **/
 
 trait Secured {
@@ -24,24 +26,53 @@ trait Secured {
 
   def getAccountId(): Long = accountId
   def SignedAPI(accessId: Long)(f: Request[AnyContent] => Result) = Action { implicit request =>
-    val (_api_token, _empty) = Form(
-      tuple(
-        "api_token" -> optional(text),
-        "empty" -> optional(text)
-      )
-    ).bindFromRequest.get
-    val api_token = _api_token.getOrElse("")
-    if(api_token.length == 0 || !Account.verifyToken(accessId, api_token, request)){
-      val domain = Play.configuration.getString("mintpresso.panel.domain").getOrElse("localhost")
-      val remoteAddress = Play.configuration.getString("mintpresso.panel.address").getOrElse("127.0.0.1")
-      if(request.domain == domain && request.remoteAddress == remoteAddress){
-        f(request)
-      } else {
-        Logger.info("Access Denied: domain " + request.domain + " == " + domain + " && remoteAddress " + request.remoteAddress + " == " + remoteAddress)
-        Results.Forbidden
-      }
-    } else {
+    val domain = Play.configuration.getString("mintpresso.panel.domain").getOrElse("localhost")
+    val remoteAddress = Play.configuration.getString("mintpresso.panel.address").getOrElse("127.0.0.1")
+    if(request.domain == domain && request.remoteAddress == remoteAddress){
       f(request)
+    }else{
+      request.queryString.get("api_token").flatMap(_.headOption) match {
+        case Some(token) => {
+          if(token.length == 0){
+            Logger.info("Access Denied: zero-length token")
+            Results.Forbidden
+          }else{
+            val ll = accessId.toString.length
+            val i = token.substring(0, ll)
+            if(i != accessId.toString){
+              Logger.warn("Given ID is unmatched with the id signed in token.")
+              Results.Forbidden
+            }else{
+              Results.Async {
+                MintpressoAPI("internal").findByTypeAndIdentifier("token", token).map { res =>
+                  res.status match {
+                    case 200 => {
+                      val json = Json.parse(res.body)
+                      val expired = (json \ "point" \ "data" \ "expired").as[Boolean]
+                      val urls: List[String] = (json \ "point" \ "data" \ "url").as[String].split('|').toList
+                      if(expired){
+                        Logger.info("FAILED - expired token " + res.body)
+                        Results.Forbidden
+                      }else{
+                        Logger.info("COMPARE DISABLED: domain(" + request.domain + "), remoteAddress(" + request.remoteAddress + "), filter(" + urls.toString + ") ")
+                        f(request)
+                      }
+                    }
+                    case _ => {
+                      Logger.info("FAILED - find token")
+                      Results.Forbidden
+                    }
+                  }
+                }
+              }
+            }
+          }
+        }
+        case None => {
+          Logger.info("Access Denied: no token given")
+          Results.Forbidden
+        }
+      }
     }
   }
   def LocalAPI(f: Request[AnyContent] => Result) = Action { implicit request =>
