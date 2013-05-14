75964b544a637123929c4de0bea1db0be59a88da
Jinhyuk Lee <eces@mstock.org>
2013-05-14 20:53:33 +0900

    Remove framework

--- framework/src/play/src/main/java/play/package-info.java
+++
@@ -1,7 +0,0 @@
-/**
- * Provides the Play framework's publicly accessible Java API.
- *
- * <h3>Play 2.1</h3>
- * <a href="http://www.playframework.org">http://www.playframework.org</a>
- */
-package play;
