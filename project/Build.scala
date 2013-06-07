64f967692fee99c606230cafb8c991f4f24ffe1b
Jinhyuk Lee <eces@mstock.org>
2013-06-08 03:09:38 +0900

    Update sbt 0.12.3

--- project/Build.scala
+++ project/Build.scala
@@ -17,7 +17,7 @@ object ApplicationBuild extends Build {
 
 
   val main = play.Project(appName, appVersion, appDependencies).settings(
-    // Add your own project settings here      
+    
   ) dependsOn (
     RootProject(uri(conf.getString("affogato.repository")))
   )
