fe1dc7fff920b7ed1a94c1e885bfecb97ffaa0a4
Jinhyuk Lee <eces@mstock.org>
2013-06-20 23:27:37 +0900

    Alter table for data, updatedAt

---
+++ conf/evolutions/default/11.sql
@@ -0,0 +1,10 @@
+# --- !Ups
+ALTER TABLE `edges` ADD COLUMN `data` VARCHAR(10240) NOT NULL;
+ALTER TABLE `edges` ADD COLUMN `updatedAt` DATETIME(6) NOT NULL;
+
+SET SQL_SAFE_UPDATES = 0;
+UPDATE `edges` set `updatedAt` = `createdAt`;
+SET SQL_SAFE_UPDATES = 1;
+ 
+# --- !Downs
+ALTER TABLE `edges` DROP COLUMN `updatedAt`;
